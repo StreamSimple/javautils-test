c9e11a355ae0b6331252a20be5329f5e6abe7eed
Timothy Farkas <tim@simplifi.it>
2017-11-01 18:06:04 -0700

    - Made the getResourceAsFile method a bit easier to use.

--- src/main/java/com/simplifi/it/javautils/testutils/ResourceUtils.java
+++ src/main/java/com/simplifi/it/javautils/testutils/ResourceUtils.java
@@ -4,18 +4,30 @@ import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public final class ResourceUtils
 {
+  private static final Path ROOT_PATH = Paths.get("/");
+
   private ResourceUtils()
   {
   }
 
-  public static File getResourceAsFile(Path resourcePath)
+  public static File getResourceAsFile(final Path resourcePath)
   {
+    if (resourcePath.isAbsolute()) {
+      final String message = String.format("The given path [%s] is absolute, paths must be relative to " +
+          "where resources are stored.", resourcePath);
+    }
+
+    final String resourcePathString = ROOT_PATH
+        .resolve(resourcePath)
+        .toString();
+
     try {
       final URI resourceURI = ResourceUtils.class
-          .getResource(resourcePath.toString())
+          .getResource(resourcePathString)
           .toURI();
       return new File(resourceURI);
     } catch (URISyntaxException e) {
