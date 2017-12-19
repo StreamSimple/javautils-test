dd674caeba15e848f85dc0cf2efd46a05ad2fda6
Timothy Farkas <tim@streamsimple.io>
2017-12-18 22:32:32 -0800

    - Added plugins to pom  - Fixed checkstyle errors  - Added license headers

--- src/main/java/com/streamsimple/javautils/testutils/DirTestWatcher.java
+++ src/main/java/com/streamsimple/javautils/testutils/DirTestWatcher.java
@@ -1,14 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.streamsimple.javautils.testutils;
-
-import com.streamsimple.commons.io.FileUtils;
-import org.junit.rules.TestWatcher;
-import org.junit.runner.Description;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 
-public class DirTestWatcher extends TestWatcher {
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+
+import com.streamsimple.commons.io.FileUtils;
+
+public class DirTestWatcher extends TestWatcher
+{
   private String dirPath;
   private File dir;
   private boolean deleteAtEnd = false;
@@ -24,7 +43,8 @@ public class DirTestWatcher extends TestWatcher {
   }
 
   @Override
-  protected void starting(Description description) {
+  protected void starting(Description description)
+  {
     String methodName = description.getMethodName();
     String className = description.getClassName();
     dir = Paths.get(".", "target", className, methodName).toFile();
@@ -35,12 +55,14 @@ public class DirTestWatcher extends TestWatcher {
   }
 
   @Override
-  protected void finished(Description description) {
+  protected void finished(Description description)
+  {
     deleteDirEnd();
   }
 
   @Override
-  protected void failed(Throwable e, Description description) {
+  protected void failed(Throwable e, Description description)
+  {
     deleteDirEnd();
   }
 
@@ -60,18 +82,21 @@ public class DirTestWatcher extends TestWatcher {
     }
   }
 
-  public File makeSubDir(String subDirName) {
+  public File makeSubDir(String subDirName)
+  {
     File subDir = new File(dir, subDirName);
     subDir.mkdirs();
     return subDir;
   }
 
   @Deprecated
-  public String getDirPath() {
+  public String getDirPath()
+  {
     return dirPath;
   }
 
-  public File getDir() {
+  public File getDir()
+  {
     return dir;
   }
 
